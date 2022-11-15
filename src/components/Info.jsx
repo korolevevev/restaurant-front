1ba095c36091f732308af48ca693e09753f37564
korolevevev <korolev44kos@gmail.com>
2022-11-15 16:06:34 +0300

    added some fixes

--- src/components/Info.jsx
+++ src/components/Info.jsx
@@ -1,7 +1,7 @@
 export const Info = () => {
     return (<div>
-        <h4 className={"text-white mt-3"}>Работу выполнил: Королёв Илья Алексеевич</h4>
-        <h4 className={"text-white mt-3"}>Предмет: Программная инженерия</h4>
-        <h4 className={"text-white mt-3"}>Сентябрь-ноябрь 2022</h4>
+        <h4 className={"text-white mt-3"}>Сделано: Илья Королёв</h4>
+        <a href="https://github.com/korolevevev/restaurant-front/" className={"text-white mt-3"}>Github фронтенд</a>
+        <a href="https://github.com/korolevevev/restaurant-back/" className={"text-white mt-3"}>Github бэкенд</a>
     </div>);
 }
--- src/index.js
+++ src/index.js
@@ -7,7 +7,7 @@ import {BrowserRouter} from "react-router-dom";
 
 ReactDOM.render(
     <React.StrictMode>
-        <BrowserRouter>
+        <BrowserRouter basename={process.env.PUBLIC_URL}>
             <App/>
         </BrowserRouter>
 
@@ -15,7 +15,4 @@ ReactDOM.render(
     document.getElementById('root')
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 reportWebVitals();
