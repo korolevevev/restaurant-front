48f6dc1c77b095763ba3a0d63ef1f31357c53486
korolevevev <korolev44kos@gmail.com>
2022-11-15 15:34:34 +0300

    small fixes

--- src/components/Info.jsx
+++ src/components/Info.jsx
@@ -1,9 +1,7 @@
 export const Info = () => {
     return (<div>
         <h4 className={"text-white mt-3"}>Работу выполнил: Королёв Илья Алексеевич</h4>
-        <h4 className={"text-white mt-3"}>Тема курсовой работы: Информационно-справочная система ресторана</h4>
-        <h6 className={"text-grey mt-3"}>Финансовый университет при Правительстве РФ</h6>
-        <h6 className={"text-grey mt-3 "}>Факультет информационных технологий и анализа больших данных</h6>
-        <h6 className={"text-grey mt-3 "}>Москва, 2021</h6>
+        <h4 className={"text-white mt-3"}>Предмет: Программная инженерия</h4>
+        <h4 className={"text-white mt-3"}>Сентябрь-ноябрь 2022</h4>
     </div>);
 }
--- src/components/Order/Bills.jsx
+++ src/components/Order/Bills.jsx
@@ -1,11 +1,19 @@
 import React from 'react';
 import {useHistory} from "react-router";
+import {HorizontalGridLines, LineSeries, VerticalGridLines, XAxis, XYPlot, YAxis} from "react-vis";
 function Bills({el}) {
     let history = useHistory();
     if(!el.dishList){
         history.push('/bill')
         alert("Заказ пуст")
         return null
+    }
+    let data = []
+    if (el.dishList) {
+        data = el.dishList.map((el1) => {return {x: el1.dish, y: (el1.price)}})
+        console.log(data)
+    }else{
+        history.push("/carshowroom")
     }
     return (
         <div className={"text-white mt-5"}>
@@ -16,6 +24,20 @@ function Bills({el}) {
             <div className={'row'}><div className={'col'}>Номер стола: {el.table_id}</div></div>
             <div className={'row'}><div className={'col'}>Оплачен: {el.paid ? 'Да' : 'Нет'}</div></div>
             <div className={'row'}><div className={'col'}>Сумма заказа: {el.total}</div></div>
+            <div style={{ backgroundColor: "black", borderRadius: "10px"}}>
+                <XYPlot
+                    xType="ordinal"
+                    width={1000}
+                    height={500}>
+                    <VerticalGridLines />
+                    <HorizontalGridLines />
+                    <XAxis title="Название блюда" style={{stroke: "#fafafa"}}/>
+                    <YAxis title="Цена, руб"  style={{stroke: "#fafafa"}}/>
+                    <LineSeries
+                        data={data}
+                        style={{stroke: 'violet', strokeWidth: 1}}/>
+                </XYPlot>
+            </div>
         </div>
     );
 }
